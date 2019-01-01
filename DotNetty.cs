d3cf4b81fd7fff34d8679ef11a7f76a678dfa0fe
lsj <lsj@lsj.com>
2019-01-01 16:55:56 +0800

    添加netty协议

---
+++ DotNetty.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FootStone.Protocol
+{
+    [Serializable]
+    public class MsgHandShakeRequest
+    {
+        public string playerId;
+    }
+
+    [Serializable]
+    public class MsgHandShakeResponse
+    {
+        public string channelId;
+    }
+}
